9e9aef8996371e1961a3af04906b109632d8606c
fabianlagos <fabian.lagos.b@gmail.com>
2017-06-15 01:21:05 -0400

    Agregado crear producto

--- web2py/applications/control_inventario_dw/controllers/gestion_productos.py
+++ web2py/applications/control_inventario_dw/controllers/gestion_productos.py
@@ -4,7 +4,7 @@ def productos():
 
     db.producto.id.readable = False
 
-    grid = SQLFORM.grid(consulta, csv=False, links = [lambda row: A(' agregar a inventario',_href=URL("agregarainventario",args=[row.id]), _class="btn btn-default glyphicon glyphicon-plus")])
+    grid = SQLFORM.grid(consulta, create=False, csv=False, links = [lambda row: A(' agregar a inventario',_href=URL("agregarainventario",args=[row.id]), _class="btn btn-default glyphicon glyphicon-plus")])
 
     return dict(grid=grid)
 
@@ -38,9 +38,13 @@ def crear_producto():
         db.producto,
         Field('Categoria', requires=IS_IN_SET(categorias)) )
     if form.process().accepted:
-        response.flash = 'form accepted'
-        session.your_name = form.vars.your_name
-        session.your_image = form.vars.your_image
+        id_producto = db.producto.insert(**db.producto._filter_fields(form.vars))
+        id_categoria = db(db.categoria.nombre == form.vars.Categoria).select(db.categoria.id)[0].get('id')
+        db.categoria_producto.insert(id_producto=id_producto, id_categoria=id_categoria)
+        redirect(URL('productos'))
+
+        response.flash = "Producto agregado correctamente"
+
     elif form.errors:
         response.flash = 'form has errors'
     return dict(form=form)
