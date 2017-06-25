02b86211f632c394a414cae28d7aa4bdbc079d64
fabianlagos <fabian.lagos.b@gmail.com>
2017-06-25 04:33:17 -0400

    login nuevo

--- web2py/applications/control_inventario_dw/controllers/default.py
+++ web2py/applications/control_inventario_dw/controllers/default.py
@@ -8,7 +8,7 @@
 # - download is for downloading files uploaded in the db (does streaming)
 # -------------------------------------------------------------------------
 
-
+@auth.requires_login()
 def index():
     """
     example action using the internationalization operator T and flash
